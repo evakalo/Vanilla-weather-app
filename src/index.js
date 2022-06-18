5db9952aff7b9b801603e19ded49f879d83722f2
evakalo <99054186+evakalo@users.noreply.github.com>
2022-06-18 14:19:32 +0200

    showing current temperature

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-alert("Hello World!");
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let weatherDescription = document.querySelector("#description");
+  weatherDescription.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = response.data.wind.speed;
+}
+
+let apiKey = "87fada2310a69c86ce747c4ec875050c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
